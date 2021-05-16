ff21f78cec23f267096f32405d7a316a06f297a4
IcemenUa <bogdan.senkiv@gmail.com>
2021-05-16 09:28:05 +0300

    new routing ; added new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FilmsComponent } from './pages/films/films.component';
+import { WatchedFilmsComponent } from './pages/watched-films/watched-films.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'Films' },
+  { path: 'Films', component: FilmsComponent },
+  { path: 'WatchedFilms', component: WatchedFilmsComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ASideComponent } from './layouts/a-side/a-side.component';
 import { FilmsComponent } from './pages/films/films.component';
+import { FilmPageComponent } from './pages/film-page/film-page.component';
+import { WatchedFilmsComponent } from './pages/watched-films/watched-films.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
 @NgModule({
-  declarations: [
-    AppComponent,
-    ASideComponent,
-    FilmsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ASideComponent, FilmsComponent, WatchedFilmsComponent, FilmPageComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule, ModalModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
